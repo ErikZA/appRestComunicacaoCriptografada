a483825a592cdad1d0513cde7af4172f8496ce92
ErikZambeli <erik_12_nf@live.com>
2019-05-28 01:50:40 -0300

    Final

--- src/main/java/com/followzuoComunicacaoCriptografada/ComunicacaoCriptografada/controller/MensagenResource.java
+++ src/main/java/com/followzuoComunicacaoCriptografada/ComunicacaoCriptografada/controller/MensagenResource.java
@@ -47,7 +47,7 @@ public class MensagenResource {
         //System.out.println(request.toString());
         String encrypt_string = request.getParameter("fzupresponse");
         String usermessage = fll.decrypt(encrypt_string);
-        List<String> user = Arrays.asList(usermessage.split(","));
+        List<String> user = Arrays.asList(usermessage.split(";"));
         System.out.println("------------------------\n\n\n"+user.toString()+"------------------------\n\n\n");
         List<BancoDeMensagens> lista = BancoDeMensagensRepository.findAllByUserID(user.get(1));
         if (user.get(2).equalsIgnoreCase("ListAll()")) {
@@ -66,7 +66,7 @@ public class MensagenResource {
                     "FZUP_COMMAND = smsg",
                     "FZUP_USER    = " + user.get(1),
                     "FZUP_MSGTEXT = MENSAGEM SALVA NO REPOSITORIO"});
-            System.out.println("------------------------\n\n\n"+user.toString()+" -- Salva Mensagem------------------------\n\n\n");
+            System.out.println("------------------------\n\n\n"+user.toString()+" -- Salva Mensagem"+user.get(1)+"------------------------\n\n\n");
             return result;
         }
     }
